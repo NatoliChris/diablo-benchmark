edb6083b8165fae26e688b6da203a7f2c557289e
Chris <christopher.natoli@sydney.edu.au>
2020-06-04 17:06:52 +1000

    add check for values

--- core/parsers/benchparser_test.go
+++ core/parsers/benchparser_test.go
@@ -2,6 +2,7 @@ package parsers
 
 import (
 	"diablo-benchmark/core/configs"
+	"fmt"
 	"testing"
 )
 
@@ -52,5 +53,58 @@ func TestParseSampleBenchConfig(t *testing.T) {
 		check("description", "descriptions", bConfig.Description)
 		check("txtype", configs.TxTypeSimple, bConfig.TxInfo.TxType)
 		check("fullTxLength", 30, len(bConfig.TxInfo.Intervals))
+	})
+
+	t.Run("test filling values onerate", func(t *testing.T) {
+		exampleOneRateConfig := `
+name: "sample"
+description: "descriptions"
+bench:
+  type: "simple"
+  txs:
+    0: 70
+    10: 70
+    40: 70
+`
+		sampleBytes := []byte(exampleOneRateConfig)
+
+		bConfig, err := parseBenchYaml(sampleBytes)
+
+		if err != nil {
+			t.Errorf("failed to parse yaml, err: %s", err)
+		}
+
+		for i := 0; i < 40; i++ {
+			check(fmt.Sprintf("oneRate array [%d]", i),
+				70,
+				bConfig.TxInfo.Intervals[i],
+			)
+		}
+	})
+
+	t.Run("test non_zero provided should start at 0", func(t *testing.T) {
+		exampleNonZeroStart := `
+name: "sample"
+description: "descriptions"
+bench:
+  type: "simple"
+  txs:
+    10: 10
+    40: 70
+`
+		sampleBytes := []byte(exampleNonZeroStart)
+
+		bConfig, err := parseBenchYaml(sampleBytes)
+
+		if err != nil {
+			t.Errorf("failed to parse yaml, err: %s", err)
+		}
+
+		for i := 0; i < 10; i++ {
+			check(fmt.Sprintf("non-Zero starting [%d]", i),
+				i,
+				bConfig.TxInfo.Intervals[i],
+			)
+		}
 	})
 }
