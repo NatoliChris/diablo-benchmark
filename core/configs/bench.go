0fbfd6ade99de23b6f315de85171285a45420bdf
Chris <christopher.natoli@sydney.edu.au>
2020-06-04 16:57:35 +1000

    update bench struct yaml tags for v3

--- core/configs/bench.go
+++ core/configs/bench.go
@@ -2,13 +2,13 @@ package configs
 
 // Benchmark configuration structure, all the information from the event.
 type BenchConfig struct {
-	Name        string    `yaml:name`        // Name of the benchmark
-	Description string    `yaml:description` // Description of what it is
-	TxInfo      BenchInfo `yaml:bench`       // Benchmark transaction information
+	Name        string    `yaml:"name"`        // Name of the benchmark
+	Description string    `yaml:"description"` // Description of what it is
+	TxInfo      BenchInfo `yaml:"bench,flow"`  // Benchmark transaction information
 }
 
 // Benchmark information about transaction intervals and types.
 type BenchInfo struct {
-	TxType    string       `yaml:type` // Type of the transactions (simple, contract)
-	Intervals TPSIntervals `yaml:txs`  // Transactions
+	TxType    string       `yaml:"type"` // Type of the transactions (simple, contract)
+	Intervals TPSIntervals `yaml:"txs"`  // Transactions
 }
