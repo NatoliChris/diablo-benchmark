1697b273268b3ad830f14aa65e7d7422b9af70a9
Chris <christopher.natoli@sydney.edu.au>
2020-06-13 15:43:02 +1000

    Add generic transaction benchmark information

--- blockchains/generictypes.go
+++ blockchains/generictypes.go
@@ -9,3 +9,12 @@ type GenericBlock struct {
 	Timestamp         uint64 // Unix timestamp of the block
 	TransactionNumber int    // Number of transactions included in the block
 }
+
+// Generic information about the transaction, stores hash, sent time and
+// time that it was mined into a block
+type TransactionBenchmarkInformation struct {
+	Hash            string // Unique transaction hash
+	SentTime        uint64 // Time that the transaction request was sent
+	RequestResponse uint64 // Response time that was returned
+	BlockTime       uint64 // Time that it was mined into a block.
+}
