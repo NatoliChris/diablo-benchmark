1b78a8b2519d51adfc2bd2133df66b812c1bd0a3
Chris <christopher.natoli@sydney.edu.au>
2020-06-15 17:02:29 +1000

    Work on workload generator interface

---
+++ blockchains/workloadgenerators/workloadgeneratorinterface.go
@@ -0,0 +1,17 @@
+package workloadgenerators
+
+import "diablo-benchmark/core/configs"
+
+type WorkloadGenerator interface {
+	// Creates a transaction to deploy the contract
+	CreateContractDeployTransaction(contractPath string, key configs.ChainKey) ([]byte, error)
+
+	// Creates an interaction with the contract
+	CreateContractInteraction(contractAddress string, contractFunction string, params map[string]interface{}) ([]byte, error)
+
+	// Create a signed transaction that returns the bytes
+	CreateSignedTransaction(to string, value string, data []byte, key configs.ChainKey) ([]byte, error)
+
+	// Generate the workload, returning the slice of transactions. [clientID = [ list of transactions ] ]
+	GenerateWorkload(numClients int, numTransactions int, transactionInformation map[string]interface{}) ([][][]byte, error)
+}
