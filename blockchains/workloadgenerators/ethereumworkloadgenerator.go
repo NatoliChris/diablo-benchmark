0cda30fd9fb4dc6a17461aa59b33eed307638d16
Chris <christopher.natoli@sydney.edu.au>
2020-06-17 18:22:03 +1000

    Update parser and generator

--- blockchains/workloadgenerators/ethereumworkloadgenerator.go
+++ blockchains/workloadgenerators/ethereumworkloadgenerator.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"diablo-benchmark/core/configs"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
@@ -17,13 +19,14 @@ type EthereumWorkloadGenerator struct {
 	SuggestedGasPrice *big.Int
 	BenchConfig       configs.BenchConfig
 	Nonces            map[string]uint64
+	ChainID           *big.Int
 }
 
 // Sets the suggested gas price and sets up a small connection to get information from the blockchain.
-func (e *EthereumWorkloadGenerator) Init(chainConfig configs.ChainConfig, benchConfig configs.BenchConfig) error {
+func (e *EthereumWorkloadGenerator) Init(chainConfig *configs.ChainConfig, benchConfig *configs.BenchConfig) error {
 
 	// Connect to the blockchain
-	c, err := ethclient.Dial(fmt.Sprintf("ws://%s:%s", chainConfig.Nodes[0][0], chainConfig.Nodes[0][1]))
+	c, err := ethclient.Dial(fmt.Sprintf("ws://%s", chainConfig.Nodes[0]))
 
 	if err != nil {
 		return err
@@ -37,11 +40,18 @@ func (e *EthereumWorkloadGenerator) Init(chainConfig configs.ChainConfig, benchC
 		return err
 	}
 
+	// Chain ID
+	chainID, err := e.ActiveConn.NetworkID(context.Background())
+	if err != nil {
+		return err
+	}
+	e.ChainID = chainID
+
 	// nonces
 	e.Nonces = make(map[string]uint64, 0)
 
 	for _, key := range chainConfig.Keys {
-		v, err := e.ActiveConn.NonceAt(context.Background(), common.HexToAddress(key.Address), -1)
+		v, err := e.ActiveConn.PendingNonceAt(context.Background(), common.HexToAddress(key.Address))
 		if err != nil {
 			return err
 		}
@@ -70,13 +80,30 @@ func (e *EthereumWorkloadGenerator) CreateSignedTransaction(to string, value str
 		return []byte{}, err
 	}
 
-	fromAddress := common.HexToAddress(key.Address)
-	toAddress := common.HexToAddress(to)
+	biVal := big.NewInt(0)
 
+	biVal, ok := biVal.SetString(value, 10)
+
+	if !ok {
+		return []byte{}, errors.New("failed to parse value to bigInt")
+	}
+
+	// Get the transaction fields
+	toAddress := common.HexToAddress(to)
+	gasLimit := uint64(21000)
+
+	tx := types.NewTransaction(e.Nonces[key.Address], toAddress, biVal, gasLimit, e.SuggestedGasPrice, data)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(e.ChainID), priv)
+	if err != nil {
+		return []byte{}, nil
+	}
+
+	// Return the transaction in bytes ready to send to the clients.
+	return signedTx.MarshalJSON()
 }
 
 // Generate the workload, returning the slice of transactions. [clientID = [ list of transactions ] ]
-func (e *EthereumWorkloadGenerator) GenerateWorkload(numClients int, numTransactions int, transactionInformation map[string]interface{}) ([][][]byte, error) {
+func (e *EthereumWorkloadGenerator) GenerateWorkload(numClients int, numTransactionsPerClient int, transactionInformation map[string]interface{}) ([][][]byte, error) {
 	clientWorkloads := make([][][]byte, 0)
 
 	return clientWorkloads, nil
