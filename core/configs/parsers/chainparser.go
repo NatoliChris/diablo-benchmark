35e23e7f6b6c2ca2f1320ac8ee9ff3a88fa21131
Chris <christopher.natoli@sydney.edu.au>
2020-07-30 13:21:45 +1000

    Create utils to remove potential import loop

--- core/configs/parsers/chainparser.go
+++ core/configs/parsers/chainparser.go
@@ -1,10 +1,7 @@
 package parsers
 
 import (
-	"diablo-benchmark/blockchains/workloadgenerators"
 	"diablo-benchmark/core/configs"
-	"errors"
-	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 )
@@ -35,20 +32,4 @@ func parseChainYaml(fileContents []byte) (*configs.ChainConfig, error) {
 	}
 
 	return &chainConfig, nil
-}
-
-func GetWorkloadGenerator(config *configs.ChainConfig) (workloadgenerators.WorkloadGenerator, error) {
-	switch config.Name {
-	case "ethereum":
-		// Return the ethereum workload generator
-		// TODO get the type of the ethereum workload generator
-		var wg workloadgenerators.WorkloadGenerator
-		wg = &workloadgenerators.EthereumWorkloadGenerator{}
-		return wg, nil
-
-	default:
-		zap.L().Warn("unknown chain defined in config",
-			zap.String("chain_name", config.Name))
-		return nil, errors.New("unknown chain when parsing config")
-	}
 }
