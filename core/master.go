e15862563370fc3bc91a5fef5281dcc459b3ebb7
Chris <christopher.natoli@sydney.edu.au>
2020-07-30 19:52:20 +1000

    Update master workflow with new workload generator and abstractions

--- core/master.go
+++ core/master.go
@@ -1,8 +1,10 @@
 package core
 
 import (
+	"diablo-benchmark/blockchains"
 	"diablo-benchmark/blockchains/workloadgenerators"
 	"diablo-benchmark/communication"
+	"diablo-benchmark/core/configs"
 	"go.uber.org/zap"
 )
 
@@ -10,23 +12,46 @@ import (
 type Master struct {
 	Server            *communication.MasterServer // TCP server identified with the master for all clients to connect to
 	workloadGenerator workloadgenerators.WorkloadGenerator
+	benchmarkConfig   *configs.BenchConfig
+	chainConfig       *configs.ChainConfig
 }
 
 // Initialise the master server and return an instance of the master
 // This will be passed back to the main
-func InitMaster(listenAddr string, expectedClients int, wg workloadgenerators.WorkloadGenerator) *Master {
+func InitMaster(listenAddr string, expectedClients int, wg workloadgenerators.WorkloadGenerator, bConfig *configs.BenchConfig, cConfig *configs.ChainConfig) *Master {
 	s, err := communication.SetupMasterTCP(listenAddr, expectedClients)
 	if err != nil {
 		// TODO remove panic
 		panic(err)
 	}
 
-	return &Master{Server: s, workloadGenerator: wg}
+	return &Master{
+		Server:            s,
+		workloadGenerator: wg,
+		benchmarkConfig:   bConfig,
+		chainConfig:       cConfig,
+	}
 }
 
 // Main functionality to run
 // Holds the majority of the work
 func (ms *Master) Run() {
+	// First, set up the blockchain
+	err := ms.workloadGenerator.BlockchainSetup()
+
+	if err != nil {
+		zap.L().Error("encountered error with blockchain setup",
+			zap.String("error", err.Error()))
+		return
+	}
+
+	// Next, init the workload generator
+	err = ms.workloadGenerator.InitParams()
+	if err != nil {
+		zap.L().Error("encountered error with workloadgenerator InitParams",
+			zap.String("error", err.Error()))
+		return
+	}
 
 	// Get the client connections ready
 	clientReadyChannel := make(chan bool, 1)
@@ -53,14 +78,38 @@ func (ms *Master) Run() {
 	zap.L().Info("Benchmark clients all connected.",
 		zap.Int("clients", len(ms.Server.Clients)))
 
+	// Set up the blockchain information
+
 	// Step 2: Blockchain type (tells which interface they should be using)
-	ms.Server.SendBlockchainType()
+	// get the blockchain byte
+	bcMessage, err := blockchains.MatchStringToMessage(ms.chainConfig.Name)
+
+	if err != nil {
+		ms.Server.CloseClients()
+		ms.Server.Close()
+	}
+
+	errs = ms.Server.SendBlockchainType(bcMessage)
+
+	if errs != nil {
+		zap.L().Error("failed to send blockchain type",
+			zap.Strings("errors", errs))
+		ms.Server.CloseClients()
+		ms.Server.Close()
+		return
+	}
 
 	// Step 3: Prepare the workload for the benchmark
 	// TODO: generate workloads
+	workload, err := ms.workloadGenerator.GenerateWorkload()
+
+	if err != nil {
+		zap.L().Error("failed to generate workload",
+			zap.String("error", err.Error()))
+	}
 
 	// Step 4: Distribute benchmark
-	ms.Server.SendWorkload()
+	ms.Server.SendWorkload(workload)
 
 	// Step 5: run the bench
 	ms.Server.RunBenchmark()
@@ -74,10 +123,4 @@ func (ms *Master) Run() {
 	// Step 8: Close all connections
 	ms.Server.CloseClients()
 	ms.Server.Close()
-}
-
-func (ms *Master) GenerateWorkloads() ([][]byte, error) {
-	// 1/ work out the total number of clients.
-
-	return nil, nil
 }
