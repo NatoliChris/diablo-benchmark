90da4cd2b0128c3eea4ae61867628e736a4faf32
Chris <christopher.natoli@sydney.edu.au>
2020-08-05 22:54:41 +1000

    Move generic block to types to remove import cycles

---
+++ blockchains/clientinterfaces/types.go
@@ -0,0 +1,13 @@
+// Implements the generic types for blockchain artefacts
+package clientinterfaces
+
+// Generic block for the blockchains, this may or may not be fully filled.
+// This should be extended to accompany for other blockchains but MUST retain
+// base functionality for other chains.
+type GenericBlock struct {
+	Hash              string   // Unique identifier for the block
+	Index             uint64   // Height of the block as an index
+	Timestamp         uint64   // Unix timestamp of the block
+	TransactionNumber int      // Number of transactions included in the block
+	TransactionHashes []string // The hash of each transaction included in the block
+}
