e31ad02ae43ab6ea2ed8e9a09223e0ee801f0db9
Chris <christopher.natoli@sydney.edu.au>
2020-08-07 15:34:09 +1000

    Implement workload handler in worker script

--- core/worker.go
+++ core/worker.go
@@ -5,41 +5,35 @@ import (
 	"diablo-benchmark/blockchains/clientinterfaces"
 	"diablo-benchmark/communication"
 	"diablo-benchmark/core/configs"
+	"diablo-benchmark/core/handlers"
 	"encoding/binary"
 	"encoding/json"
-	"fmt"
 	"go.uber.org/zap"
 )
 
 type Worker struct {
-	ID          int                                  // This worker's unique ID
-	ChainConfig *configs.ChainConfig                 // Chain configuration
-	Blockchain  clientinterfaces.BlockchainInterface // Blockchain Interface
-	MasterComms *communication.ConnClient            // Connection to the master
-	Workload    []interface{}                        // The workload after it has been parsed
+	ID              int                                  // This worker's unique ID
+	ChainConfig     *configs.ChainConfig                 // Chain configuration
+	Blockchain      clientinterfaces.BlockchainInterface // Blockchain Interface
+	MasterComms     *communication.ConnClient            // Connection to the master
+	WorkloadHandler *handlers.WorkloadHandler            // Workload Handler
 }
 
 // Create a new worker, set up the things we need.
 func NewWorker(config *configs.ChainConfig, masterAddress string) (*Worker, error) {
-	// First make a new worker
-	w := Worker{ChainConfig: config}
-
 	// Set up the communication
 	c, err := communication.SetupClientTCP(masterAddress)
 	if err != nil {
 		zap.L().Error("failed to connect to master server")
 		return nil, err
 	}
-	w.MasterComms = c
-	bc, err := clientinterfaces.GetBlockchainInterface(config)
-	if err != nil {
-		return nil, err
-	}
-	w.Blockchain = bc
 
+	// Log and return, ready to go!
 	zap.L().Info("Client init")
-
-	return &w, nil
+	return &Worker{
+		ChainConfig: config,
+		MasterComms: c,
+	}, nil
 }
 
 // Runs the main worker things, sets up the client and waits for the commands
@@ -65,8 +59,27 @@ func (w *Worker) Run() {
 				zap.String("CMD", "PREPARE"))
 			// It should also give us a client ID as aux value
 			w.ID = int(cmd[1])
-			w.Blockchain.Init(w.ChainConfig.Nodes)
-			err := w.Blockchain.ConnectAll(w.ID)
+			w.ID = int(binary.BigEndian.Uint32(cmd[1:5]))
+			numThreads := binary.BigEndian.Uint32(cmd[5:9])
+			// Connect le blockchains
+			var bcis []clientinterfaces.BlockchainInterface
+			for i := uint32(0); i < numThreads; i++ {
+				bc, err := clientinterfaces.GetBlockchainInterface(w.ChainConfig)
+				if err != nil {
+					w.MasterComms.ReplyERR(err.Error())
+				}
+				bcis = append(bcis, bc)
+			}
+
+			// Create the workload handler
+			wHandler := handlers.NewWorkloadHandler(
+				numThreads,
+				bcis,
+			)
+
+			w.WorkloadHandler = wHandler
+
+			err := w.WorkloadHandler.Connect(w.ChainConfig.Nodes, w.ID)
 			if err != nil {
 				w.MasterComms.ReplyERR(err.Error())
 				continue
@@ -87,11 +100,7 @@ func (w *Worker) Run() {
 				zap.String("CMD", "WORKLOAD"))
 
 			// Workload length = bytes 1-4
-			workloadLen := binary.BigEndian.Uint32(cmd[1:])
-			fmt.Println("Got cmd")
-			fmt.Println(cmd)
-			fmt.Println("length: ", workloadLen)
-
+			workloadLen := binary.BigEndian.Uint64(cmd[1:])
 			wl, err := w.MasterComms.ReadSize(workloadLen)
 
 			if err != nil {
@@ -111,8 +120,7 @@ func (w *Worker) Run() {
 				continue
 			}
 
-			parsedWL, err := w.Blockchain.ParseWorkload(unmarshaledWorkload)
-
+			err = w.WorkloadHandler.ParseWorkloads(unmarshaledWorkload)
 			if err != nil {
 				zap.L().Warn("failed to parse workload",
 					zap.String("err", err.Error()))
@@ -120,14 +128,13 @@ func (w *Worker) Run() {
 				continue
 			}
 
-			w.Workload = parsedWL
 		case communication.MsgRun[0]:
 			zap.L().Info("Got command from master",
 				zap.String("CMD", "RUN"))
-			errs := w.runBench()
+			errs := w.WorkloadHandler.RunBench()
 			if errs != nil {
 				zap.L().Warn("error during bench",
-					zap.Errors("err", errs))
+					zap.Error(err))
 				w.MasterComms.ReplyERR(err.Error())
 				continue
 			}
@@ -148,27 +155,27 @@ func (w *Worker) Run() {
 
 }
 
-func (w *Worker) runBench() []error {
-	var errs []error
-	for i := 0; i < len(w.Workload); i++ {
-		err := w.Blockchain.SendRawTransaction(w.Workload[i])
-		errs = append(errs, err)
-	}
-
-	//tNow := time.Now()
-	//for {
-	//	if w.Blockchain.NumTxDone == uint64(len(w.Workload)) {
-	//		break
-	//	}
-	//	if time.Now().Sub(tNow) > 10*time.Second {
-	//		break
-	//	}
-	//
-	//	fmt.Printf("Sent: %d, Complete: %d\n", w.Blockchain.NumTxSent, w.Blockchain.NumTxDone)
-	//	time.Sleep(1000 * time.Millisecond)
-	//}
-	//
-	//res := w.Blockchain.Cleanup()
-
-	return nil
-}
+// func (w *Worker) runBench() []error {
+// 	var errs []error
+// 	for i := 0; i < len(w.Workload); i++ {
+// 		err := w.Blockchain.SendRawTransaction(w.Workload[i])
+// 		errs = append(errs, err)
+// 	}
+//
+// 	//tNow := time.Now()
+// 	//for {
+// 	//	if w.Blockchain.NumTxDone == uint64(len(w.Workload)) {
+// 	//		break
+// 	//	}
+// 	//	if time.Now().Sub(tNow) > 10*time.Second {
+// 	//		break
+// 	//	}
+// 	//
+// 	//	fmt.Printf("Sent: %d, Complete: %d\n", w.Blockchain.NumTxSent, w.Blockchain.NumTxDone)
+// 	//	time.Sleep(1000 * time.Millisecond)
+// 	//}
+// 	//
+// 	//res := w.Blockchain.Cleanup()
+//
+// 	return nil
+// }
