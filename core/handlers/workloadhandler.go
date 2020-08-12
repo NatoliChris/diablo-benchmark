1c34bf0cb209a8a39f0e5ef3de8030841f2e5788
Chris <christopher.natoli@sydney.edu.au>
2020-08-12 18:56:55 +1000

    Update workload generator to fix account creation

--- core/handlers/workloadhandler.go
+++ core/handlers/workloadhandler.go
@@ -111,7 +111,6 @@ func (wh *WorkloadHandler) runner(blockchainInterface clientinterfaces.Blockchai
 }
 
 func (wh *WorkloadHandler) statusPrinter(stopCh chan bool) {
-
 	var timer *time.Timer = time.NewTimer(5 * time.Second)
 	for {
 		select {
