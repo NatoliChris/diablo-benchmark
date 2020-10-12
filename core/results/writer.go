277e7f95d00ace7431722f81d3a969e15dce6977
Chris <christopher.natoli@sydney.edu.au>
2020-10-12 23:35:51 +1100

    Add nicer display of results at end of benchmark

--- core/results/writer.go
+++ core/results/writer.go
@@ -88,14 +88,13 @@ func WriteResultsToFile(benchConfig string, chainConfig string, results Aggregat
 
 	ts := fmt.Sprintf("%v", time.Now().Format(time.RFC3339))
 
-	zap.L().Info(fmt.Sprintf("Saving results in %s", resultDir),
-		zap.String("timestamp", ts))
-
 	// Write the results to file
 	err := writeResults(fmt.Sprintf("%s/%s_results.json", resultDir, ts), results)
 	if err != nil {
 		return err
 	}
+
+	zap.L().Info(fmt.Sprintf("Results saved in: %s/%s_results.json", resultDir, ts))
 
 	err = copyFile(benchConfig, fmt.Sprintf("%s/%s_workload.yaml", resultDir, ts))
 	if err != nil {
@@ -110,4 +109,23 @@ func WriteResultsToFile(benchConfig string, chainConfig string, results Aggregat
 // TODO: future - this can be made to show graphs, and present the results in a much nicer way!
 func Display(results AggregatedResults) {
 
+	var secondaryThroughputs []float64
+	var secondaryLatency []float64
+	for _, v := range results.ResultsPerSecondary {
+		secondaryThroughputs = append(secondaryThroughputs, v.Throughput)
+		secondaryLatency = append(secondaryLatency, v.AverageLatency)
+	}
+
+	fmt.Println()
+	fmt.Println("--------------------------")
+	fmt.Println("Benchmark Complete")
+	fmt.Println("--------------------------")
+	fmt.Println("[*] Aggregated Stats")
+	fmt.Println(fmt.Sprintf("- Throughput: %.3f [Min: %.3f | Max: %.3f]", results.OverallThroughput, results.MinThroughput, results.MaxThroughput))
+	fmt.Println(fmt.Sprintf("- Latency: %.3f [Min: %+v | Max: %+v]", results.AverageLatency, results.MinLatency, results.MaxLatency))
+	fmt.Println("[*] Secondary Stats")
+	fmt.Println(fmt.Sprintf("- Throughputs: %.3f", secondaryThroughputs))
+	fmt.Println(fmt.Sprintf("- Latencies: %.3f", secondaryLatency))
+	fmt.Println()
+	fmt.Println()
 }
