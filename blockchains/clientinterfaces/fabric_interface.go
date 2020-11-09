e7cfdd3f9834508e38fc5fa3dd09d2487f9372f5
benoit <harold.benoit@epfl.ch>
2020-11-09 08:58:22 +0100

    fabric client interface struct infrastructure

--- blockchains/clientinterfaces/fabric_interface.go
+++ blockchains/clientinterfaces/fabric_interface.go
@@ -8,18 +8,25 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync/atomic"
+	"time"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	"go.uber.org/zap"
 )
 
 //FabricInterface is the Hyperledger Fabric implementation of the clientinterface
 // Provides functionality to communicate with the Fabric blockchain
 type FabricInterface struct {
-	Gateway   *gateway.Gateway
-	Wallet    *gateway.Wallet
-	Network   *gateway.Network
-	Contracts map[string][]*gateway.Contract
+	Gateway   *gateway.Gateway              // The Gateway manages the network interaction on behalf of the application
+	Wallet    *gateway.Wallet				// Wallet containg user identity configured for the gateway
+	Network   *gateway.Network				// Network object originating from gateway
+	Contracts map[string]*gateway.Contract  // Map of all the smart contracts contained in the network
+	TransactionInfo  map[string][]time.Time // Transaction information
+	StartTime        time.Time              // Start time of the benchmark
+	ThroughputTicker *time.Ticker           // Ticker for throughput (1s)
+	Throughputs      []float64              // Throughput over time with 1 second intervals
 	GenericInterface
 }
 
@@ -27,7 +34,8 @@ type FabricInterface struct {
 func (f *FabricInterface) Init(otherHosts []string) {
 	f.Nodes = otherHosts
 	f.NumTxDone = 0
-	f.Contracts = map[string][]*gateway.Contract{}
+	f.Contracts = make(map[string]*gateway.Contract,0)
+	f.TransactionInfo = make(map[string][]time.Time, 0)
 
 	// create the gateaway, network and contract ?
 
@@ -48,6 +56,8 @@ func (f *FabricInterface) Init(otherHosts []string) {
 			log.Fatalf("Failed to populate wallet contents: %v", err)
 		}
 	}
+
+	//TODO : function to fetch connection-profile 
 
 	ccpPath := filepath.Join(
 		"..",
@@ -77,7 +87,7 @@ func (f *FabricInterface) Init(otherHosts []string) {
 
 	contract := f.Network.GetContract("basic")
 
-	f.Contracts[contract.Name()] = []*gateway.Contract{contract}
+	f.Contracts[contract.Name()] = contract
 }
 
 // Called when the wallet hasn't been instantiated yet
@@ -128,21 +138,81 @@ func populateWallet(wallet *gateway.Wallet) error {
 // Cleanup Finishes up and performs any post-benchmark operations.
 // Can be used to format the results to parse back
 func (f *FabricInterface) Cleanup() results.Results {
+
+	// Stop the ticker
+	f.ThroughputTicker.Stop()
+
+
+	txLatencies := make([]float64, 0)
+	var avgLatency float64
+
+	var endTime time.Time
+
+	success := uint(0)
+	fails := uint(f.Fail)
+
+	for _, v := range f.TransactionInfo {
+		if len(v) > 1 {
+			txLatency := v[1].Sub(v[0]).Milliseconds()
+			txLatencies = append(txLatencies, float64(txLatency))
+			avgLatency += float64(txLatency)
+			if v[1].After(endTime) {
+				endTime = v[1]
+			}
+
+			success++
+		} else {
+			fails++
+		}
+	}
+
+	zap.L().Debug("Statistics being returned",
+		zap.Uint("success", success),
+		zap.Uint("fail", fails))
+
+	var throughput float64
+
+	if len(txLatencies) > 0 {
+		throughput = float64(f.NumTxDone) / (endTime.Sub(f.StartTime).Seconds())
+		avgLatency = avgLatency / float64(len(txLatencies))
+	} else {
+		avgLatency = 0
+		throughput = 0
+	}
+
 	return results.Results{
-		TxLatencies:       nil,
-		AverageLatency:    0,
-		MedianLatency:     0,
-		Throughput:        0,
-		ThroughputSeconds: nil,
-		Success:           0,
-		Fail:              0,
+		TxLatencies:       txLatencies,
+		AverageLatency:    avgLatency,
+		Throughput:        throughput,
+		ThroughputSeconds: f.Throughputs,
+		Success:           success,
+		Fail:              fails,
 	}
 }
+
+
+// throughputSeconds calculates the throughput over time, to show dynamic
+func (f *FabricInterface) throughputSeconds() {
+	f.ThroughputTicker = time.NewTicker(time.Second)
+	seconds := float64(0)
+
+	for {
+		select {
+		case <-f.ThroughputTicker.C:
+			seconds++
+			f.Throughputs = append(f.Throughputs, float64(f.NumTxDone-f.Fail)/seconds)
+		}
+	}
+}
+
 
 // Start handles the starting aspects of the benchmark
 // Is primarily used for setting the start time and allocating resources for
 // metrics
-func (f *FabricInterface) Start() {}
+func (f *FabricInterface) Start() {
+	f.StartTime = time.Now()
+	go f.throughputSeconds()
+}
 
 //ParseWorkload Handles the workload, converts the bytes to usable transactions.
 // This takes the worker's workload - and transitions to transactions
@@ -161,17 +231,26 @@ func (f *FabricInterface) ConnectAll(primaryID int) error {
 	return nil
 }
 
-// DeploySmartContract deploys the smart contract, provided the path to the contract to deploy
-// Returns the address of the contract
+// DeploySmartContract deploys the smart contract but it is not needed as the contract is assumed
+// to be already deployed
 func (f *FabricInterface) DeploySmartContract(tx interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-// SendRawTransaction sends the raw transaction bytes to the blockchain
-// It is safe to assume that these bytes will be formatted correctly according to the chosen blockchain.
-// The transactions are generated through the workload to relieve the signing and encoding during timed
-// benchmarks
+// SendRawTransaction sends the transaction by the gateway
 func (f *FabricInterface) SendRawTransaction(tx interface{}) error {
+	s := tx.(string)
+	result,err := f.Contracts[s].SubmitTransaction(s)
+
+	if err != nil {
+		atomic.AddUint64(&f.Fail, 1)
+		atomic.AddUint64(&f.NumTxDone, 1)
+		return err
+	}
+
+	f.TransactionInfo[string(result)] = []time.Time{time.Now()}
+	atomic.AddUint64(&f.NumTxSent, 1)
+
 	return nil
 }
 
@@ -199,11 +278,7 @@ func (f *FabricInterface) GetBlockHeight() (uint64, error) {
 	return 0, nil
 }
 
-// GetTxDone returns the number of transactions completed
-// This is already implemented with the GenericInterface
-func (f *FabricInterface) GetTxDone() uint64 {
-	return 0
-}
+
 
 // ParseBlocksForTransactions retrieves block information from start to end index and
 // is used as a post-benchmark check to learn about the block and transactions.
