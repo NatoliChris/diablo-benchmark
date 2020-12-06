fc7e5904101d502b16fd8dc75ebf29fc14072647
Chris <christopher.natoli@sydney.edu.au>
2020-12-07 00:49:01 +1100

    Add contract name; Update to allow fallback in ethereum

--- core/configs/bench.go
+++ core/configs/bench.go
@@ -7,7 +7,7 @@ package configs
 // BenchConfig provides the main benchmark configuration structure, all information about the specified workload
 type BenchConfig struct {
 	Name         string       `yaml:"name"` // Name of the benchmark.
-	Path         string       // The path of the config
+	Path         string       // The location of this benchmark file (to be used in result printing)
 	Description  string       `yaml:"description,omitempty"` // Description of what it is.
 	Threads      int          `yaml:"threads"`               // Number of threads per secondary expected.
 	Secondaries  int          `yaml:"secondaries"`           // Number of secondary machines.
@@ -30,14 +30,16 @@ type ContractParam struct {
 
 // ContractFunction implements the details for a contract function
 type ContractFunction struct {
-	Name   string          `yaml:"name"`  // The name identifier for the function e.g. storeVal(uint32)
-	Type   string          `yaml:"ftype"` // Function type: "read", "write", "deploy" (note: deploy is for the constructor).
-	Ratio  int             `yaml:"ratio"` // Percentage of the workload that this function calls take.
-	Params []ContractParam `yaml:"params,flow"`
+	Name     string          `yaml:"name"`        // The name identifier for the function e.g. storeVal(uint32)
+	Type     string          `yaml:"ftype"`       // Function type: "read", "write", "deploy" (note: deploy is for the constructor).
+	Ratio    int             `yaml:"ratio"`       // Percentage of the workload that this function calls take.
+	PayValue string          `yaml:"value"`       // Monetary value to send to the contract with this transaction, default is 0.
+	Params   []ContractParam `yaml:"params,flow"` // Parameters of the function
 }
 
 // ContractInfo defining the path and functions that would be called.
 type ContractInfo struct {
 	Path      string             `yaml:"path"`           // Path of the contract file to be deployed (e.g. Solidity File).
+	Name      string             `yaml:"name"`           // The contract name (required for multiple deployed contracts)
 	Functions []ContractFunction `yaml:"functions,flow"` // Functions that should be called.
 }
