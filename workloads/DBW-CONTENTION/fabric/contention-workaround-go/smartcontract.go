058004009736c8cbc96880454757e25318063fef
benoit <harold.benoit@epfl.ch>
2021-01-12 11:28:00 +0100

    write contention- workaround chaincode and add all bench configs for DBW-CONTENTION

--- workloads/DBW-CONTENTION/fabric/contention-workaround-go/smartcontract.go
+++ workloads/DBW-CONTENTION/fabric/contention-workaround-go/smartcontract.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"log"
+	"strconv"
 )
 
 // SmartContract provides functions for managing an Asset
@@ -14,8 +15,8 @@ const compositeName = "id~op~value"
 
 func (s *SmartContract) Update(ctx contractapi.TransactionContextInterface, id string, delta float64, operation string) error{
 
-	if operation != "+" || operation != "-"{
-		return fmt.Errorf("Operation %s is not supported", operation)
+	if operation != "+" && operation != "-"{
+		return fmt.Errorf("operation %s is not supported", operation)
 	}
 
 	deltaString := fmt.Sprintf("%f", delta)
@@ -28,18 +29,67 @@ func (s *SmartContract) Update(ctx contractapi.TransactionContextInterface, id s
 	return ctx.GetStub().PutState(compositeKey,[]byte{0x00})
 }
 
-func (s *SmartContract) Get(ctx contractapi.TransactionContextInterface){
+func (s *SmartContract) Get(ctx contractapi.TransactionContextInterface, id string) error{
+	compositeKeyIterator,err := ctx.GetStub().GetStateByPartialCompositeKey(compositeName, []string{id})
 
+	if err != nil {
+		return err
+	}
+	defer compositeKeyIterator.Close()
 
+	if !compositeKeyIterator.HasNext(){
+		return fmt.Errorf("No asset with id %s exists", id)
+	}
+
+	var finalValue float64
+
+	for compositeKeyIterator.HasNext() {
+		compositeKey, err := compositeKeyIterator.Next()
+		if err != nil {
+			return err
+		}
+
+		// from composite key {id~op~value, {id,op,value}}, we get id~op~value, {id,op,value}, err
+		_, compositeKeyParts, err := ctx.GetStub().SplitCompositeKey(compositeKey.Key)
+		if err != nil {
+			return err
+		}
+
+		op := compositeKeyParts[1]
+		deltaString := compositeKeyParts[2]
+
+		delta,err := strconv.ParseFloat(deltaString,64)
+		if err != nil {
+			return err
+		}
+
+		switch op {
+		case "+":
+			finalValue += delta
+		case "-":
+			finalValue -= delta
+		default:
+			return fmt.Errorf("operation %s is not supported", op)
+		}
+
+		err = ctx.GetStub().DelState(compositeKey.Key)
+		if err != nil {
+			return fmt.Errorf("could not delete composite key out of state, error is : %s",err)
+		}
+
+	}
+
+	//updating the ledger with the final value
+	return s.Update(ctx, id, finalValue, "+")
 }
 
 func main() {
-	assetChaincode, err := contractapi.NewChaincode(new(SmartContract))
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 	if err != nil {
 		log.Panicf("Error creating contention-workaround chaincode: %v", err)
 	}
 
-	if err := assetChaincode.Start(); err != nil {
+	if err := chaincode.Start(); err != nil {
 		log.Panicf("Error starting contention-workaround chaincode: %v", err)
 	}
 }
